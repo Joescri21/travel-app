1f7946a82b9f30de8c772ab0d6b435f8f84144ef
Joel Escribano Bonola <joescri21@gmail.com>
2026-05-02 00:07:49 -0600

    Update: se cambia colores en la paleta y se agrega el footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CountryHome } from "./features/countries/pages/country-home/country-home";
+import { Footer } from "./features/countries/components/footer/footer";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    Footer
 ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
